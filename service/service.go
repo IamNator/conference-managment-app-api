f7caaabd6f3ff2190983774db2ae749b666c7226
iamnator <natverior1@gmail.com>
2022-02-03 16:26:32 +0100

    added go generates for mocks

--- service/service.go
+++ service/service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/iamnator/conference_mgmt_sys/storage"
 )
 
+//go:generate mockgen -source service.go -destination ./mock/service.go -package mock IService
 type IService interface {
 	Login(req model.UserLoginReq) (*model.UserAuthResponse, error)
 	RegisterUser(req model.UserSignUpReq) (*model.UserAuthResponse, error)
