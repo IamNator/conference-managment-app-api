87c46a2dd04dae12cb10ba0c83ff8bbe16150148
iamnator <natverior1@gmail.com>
2022-02-04 12:31:31 +0100

    added heroku.yml file

--- storage/storage.go
+++ storage/storage.go
@@ -1,11 +1,28 @@
 package storage
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
 
 type Storage struct {
 	db *gorm.DB
 }
 
 func New() *Storage {
-	return &Storage{}
+	dsn := os.Getenv("DATABASE_URL")
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println("connected to database")
+	return &Storage{
+		db: db,
+	}
 }
